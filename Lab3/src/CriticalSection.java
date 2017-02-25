de86f1456ea6a24b8384c9eae1ef7fd0155bde47
zhuzhuzhulc <770116831@qq.com>
2017-02-25 17:35:01 -0500

    CriticalSection

--- Lab3/src/CriticalSection.java
+++ Lab3/src/CriticalSection.java
@@ -1,4 +1,14 @@
-
+import java.util.*;
 public class CriticalSection {
-
+	private boolean Voted = false;
+	
+	public void enterCS(){
+		this.Voted = true;
+	}
+	public void releaseCS(){
+		this.Voted = false;
+	}
+	public boolean isLocked(){
+		return this.Voted;
+	}
 }
