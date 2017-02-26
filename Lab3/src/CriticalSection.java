e52c8d9be8f1231cf2a6e7250dd5d81dbd8346fb
zhuzhuzhulc <770116831@qq.com>
2017-02-25 19:50:17 -0500

    cin

--- Lab3/src/CriticalSection.java
+++ Lab3/src/CriticalSection.java
@@ -1,6 +1,5 @@
 import java.util.*;
 public class CriticalSection {
-<<<<<<< HEAD
 	private boolean Voted = false;
 	private String currentProcess = null;
 	private Queue<String> requestQueue = new LinkedList<String>();
@@ -49,37 +48,6 @@ public class CriticalSection {
 	public void sendRequest(TimeStampedMessage msg){
 		this.clearSet();
 	}
-=======
-    private boolean Voted = false;
-    private String currentProcess = null;
-    private Queue requestQueue = new LinkedList<String>();
-    
-    public void enterCS(){
-        this.Voted = true;
-    }
-    public void releaseCS(){
-        this.Voted = false;
-    }
-    public boolean isLocked(){
-        return this.Voted;
-    }
-    public void handleRequest(TimeStampedMessage msg){
-        if (msg.get_kind().equals( "request")){
-            if (this.isLocked()){
-                requestQueue.add(msg.getGroupMessageOrigin());
-            }
-            else {
-                sendAck(msg);
-                this.enterCS();
-            }
-        }
-        else if (msg.get_kind().equals("release") && msg.getGroupMessageOrigin().equals(currentProcess)){
-            
-        }
-    }
-    public void sendAck(TimeStampedMessage msg){
-        
-    }
->>>>>>> origin/master
+
 }
 
